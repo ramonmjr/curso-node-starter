3b763494289ae04bf0b280d0d20bbe96a2667ae9
Ramon Marques <ramonmjr@outlook.com>
2020-02-21 06:20:03 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
+const app = express();
+
+// Permite que envio de dados na aplicação no formato de JSON
+app.use(express.json());
+// Iniciando o DB
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi', 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+requireDir('./src/models');
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+
+
+app.listen(3001);
